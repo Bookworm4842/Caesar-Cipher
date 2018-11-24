bff744cce308218ef6b14d72a6de00894bfcb1f3
Bookworm4842 <40805640+Bookworm4842@users.noreply.github.com>
2018-11-23 20:29:59 -0600

    Create initial.js

---
+++ initial.js
@@ -0,0 +1,25 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var allAlphabet = alphabet + alphabet + alphabet;
+
+function runCipher(){
+  var cipherText = $('#cypher').val();
+  var cipherOffset = $('#offset').val();
+  cipherOffset = (cipherOffset % alphabet.length);
+  var cipherFinish = '';
+
+  for(i=0; i<cipherText.length; i++){
+     var letter = cipherText[i];
+     var upper = (letter == letter.toUpperCase());
+     letter = letter.toLowerCase();
+    
+     var index = alphabet.indexOf(letter);
+     if(index == -1){
+       cipherFinish += letter;
+     } else {
+       index = ((index + cipherOffset) + alphabet.length);
+       var nextLetter = allAlphabet[index];
+       if(upper) nextLetter = nextLetter.toUpperCase();
+       cipherFinish += nextLetter;
+     }
+     
+ runCipher();
